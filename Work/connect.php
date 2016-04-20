e417b16a0686a18a8d5c6bf347c976ff14938979
armayauibrahim <a.ibrahim@rgu.ac.uk>
2016-04-20 22:34:51 +0100

    testing our page

--- Work/connect.php
+++ Work/connect.php
@@ -1,9 +1,9 @@
 <?php
 
 define (DB_SERVER,'ap-cdbr-azure-east-c.cloudapp.net');
-define (DB_USERNAME, 'baa958ae16e55a');
-define (DB_PASSWORD, 'afe8ff9e');
-define (DB_DATABASE, 'armayau1513072');
+define (DB_USERNAME, 'b48fad490ff0b1');
+define (DB_PASSWORD, '0ffa09c0');
+define (DB_DATABASE, 'allmywork');
 
 $DB = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 ?>
