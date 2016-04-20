221021c9f13b3775072ec548c86b4912533fee4e
armayauibrahim <a.ibrahim@rgu.ac.uk>
2016-04-20 22:16:01 +0100

    testing our page

--- Work/index.php
+++ Work/index.php
@@ -1,3 +1,19 @@
-<?php
-echo "Our page is working";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Test Page</title>
+</head>
+<body>
+<h2>This is a simple login page</h2>
+
+<p>Please input the following information</p>
+
+<form action="insert.php" method="post">
+<input type="text" name="firstname" placeholder="First Name">
+    <input type="text" name="lastname" placeholder="Last Name">
+    <input type="submit" value="submit">
+
+</form>
+</body>
+</html>
---
+++ Work/insert.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ArmayaI
+ * Date: 4/20/2016
+ * Time: 10:13 PM
+ */
