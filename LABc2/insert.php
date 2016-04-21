52e5ae5cdc3a7b3722c9e0f5739901544c27f233
armayauibrahim <a.ibrahim@rgu.ac.uk>
2016-04-21 18:53:22 +0100

    testing our page

---
+++ LABc2/insert.php
@@ -0,0 +1,28 @@
+<?php
+
+include("connect.php");
+
+if (empty($_POST['firstname'])||(empty($_POST['lastname']))){
+    echo "Both fields are required";
+
+}
+    else {
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+
+        $sql = "INSERT INTO users (firstname, lastname) VALUES ('$firstname', '$lastname')";
+        mysqli_query ($db, $sql);
+    }
+
+
+
+        if (mysqli_query($db,$sql)){
+            header ("location:display.php");
+        }
+        else {
+            echo "Error: " . $sql . "<br>" . mysqli_error ($db);
+        }
+
+
+
+?>
